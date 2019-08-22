9a444c6f3b91a614f9cb143d394ca0be6d3b407d
Andrew Welch <andrew@nystudio107.com>
2019-08-22 08:29:14 -0400

    Add VuePress docs
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Similar Documentation',
+    description: 'Documentation for the Similar plugin',
+    base: '/docs/similar/',
+    dest: '../../../../sites/nystudio107/web/docs/similar',
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: [
+            ['/', 'Similar plugin for Craft CMS 3.x'],
+        ]
+    }
+};
