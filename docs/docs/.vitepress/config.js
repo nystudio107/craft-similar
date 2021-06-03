d051e31a92536be0b9ffa330b1c8bc68fb33061e
Andrew Welch <andrew@keluli.local>
2021-06-03 09:07:33 -0400

    Switch to VitePress for documentation

---
+++ docs/docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'Similar Documentation',
+    description: 'Documentation for the Similar plugin',
+    base: '/docs/similar/',
+    lang: 'en-US',
+    themeConfig: {
+        repo: 'nystudio107/craft-similar',
+        docsDir: 'docs/docs',
+        docsBranch: 'v1',
+        algolia: {
+            apiKey: '',
+            indexName: 'similar'
+        },
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+    },
+};
