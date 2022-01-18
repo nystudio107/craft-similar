2dc155d882c92ef6487cde6a818e78c1b6233b8d
Andrew Welch <andrew@keluli.local>
2022-01-18 14:57:54 -0500

    refactor: Use new DocSearch appId & appKey

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    title: 'Similar Plugin Documentation',
-    description: 'Documentation for the Similar plugin',
-    base: '/docs/similar/',
-    lang: 'en-US',
-    head: [
-        ['meta', { content: 'https://github.com/nystudio107', property: 'og:see_also', }],
-        ['meta', { content: 'https://www.youtube.com/channel/UCOZTZHQdC-unTERO7LRS6FA', property: 'og:see_also', }],
-        ['meta', { content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also', }],
-    ],
-    themeConfig: {
-        repo: 'nystudio107/craft-similar',
-        docsDir: 'docs/docs',
-        docsBranch: 'v1',
-        algolia: {
-            apiKey: '8fa8568c2723a011dfce546db408b3c9',
-            indexName: 'similar'
-        },
-        editLinks: true,
-        editLinkText: 'Edit this page on GitHub',
-        lastUpdated: 'Last Updated',
-        sidebar: 'auto',
+  title: 'Similar Plugin Documentation',
+  description: 'Documentation for the Similar plugin',
+  base: '/docs/similar/',
+  lang: 'en-US',
+  head: [
+    ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.youtube.com/channel/UCOZTZHQdC-unTERO7LRS6FA', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also',}],
+  ],
+  themeConfig: {
+    repo: 'nystudio107/craft-similar',
+    docsDir: 'docs/docs',
+    docsBranch: 'develop',
+    algolia: {
+      appId: 'C2KLRMOOKQ',
+      apiKey: 'fd638c27208e8ae357f67d9a2134476c',
+      indexName: 'similar'
     },
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+    sidebar: 'auto',
+  },
 };
