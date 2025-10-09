717ed58a077b20193a7f16f96edcfc7ecd7cd987
Andrew Welch <andrew@nystudio107.com>
2025-10-09 17:51:03 -0400

    refactor: ECS code cleanup

--- src/services/Similar.php
+++ src/services/Similar.php
@@ -160,7 +160,7 @@ class Similar extends Component
                 $first = false;
                 $query->subQuery->$method(['and', [
                     'elements_sites.siteId' => $siteId,
-                    'elements.id' => $elementIds,],
+                    'elements.id' => $elementIds, ],
                 ]);
             }
         });
