31ae5096d304131c438ded00e3c356478ae24515
Kevin Abel <kevin.abel@hudl.com>
2026-07-20 16:08:34 -0500

    Revert "Merge pull request #58 from croxton/fix/limit-criteria"
    This reverts commit 867d9d5e99190589548c5aecd2aa97c98f5c95d9, reversing
    changes made to 41486fac617efbb7df7dd5becac9a5feebdf28c1.

--- src/services/Similar.php
+++ src/services/Similar.php
@@ -68,7 +68,7 @@ class Similar extends Component
         if (!isset($data['context'])) {
             throw new Exception('Required parameter `context` was not supplied to `craft.similar.find`.');
         }
-
+        
         /** @var class-string|Element $element */
         $element = $data['element'];
         $context = $data['context'];
@@ -81,19 +81,12 @@ class Similar extends Component
 
         // Get an ElementQuery for this Element
         $elementClass = is_object($element) ? $element::class : $element;
-
-        // Stash limit and remove from criteria
-        if (isset($criteria['limit'])) {
-            $this->limit = $criteria['limit'];
-            $criteria['limit'] = null;
-        }
-
         /** @var EntryQuery $query */
         $query = $this->getElementQuery($elementClass, $criteria);
 
         // Stash any orderBy directives from the $query for our anonymous function
         $this->preOrder = $query->orderBy ?? [];
-
+        $this->limit = $query->limit;
         // Extract the $tagIds from the $context
         if (is_array($context)) {
             $tagIds = $context;
@@ -189,9 +182,6 @@ class Similar extends Component
             /** @phpstan-ignore-next-line */
             usort($elements, static fn($a, $b) => $a->count < $b->count ? 1 : ($a->count == $b->count ? 0 : -1));
         }
-        if ($this->limit) {
-            $elements = array_slice($elements, 0, $this->limit);
-        }
 
         return $elements;
     }
@@ -219,6 +209,7 @@ class Similar extends Component
         }
 
         $query->subQuery->limit(null); // inner limit to null -> fetch all possible entries, sort them afterwards
+        $query->query->limit($this->limit); // or whatever limit is set
 
         $query->subQuery->groupBy(['elements.id', 'elements_sites.id']);
 
